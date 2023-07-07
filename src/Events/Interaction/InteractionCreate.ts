34d6a84838769e192a9ab14ecf006ab660481469
Tian <marlonrivera12321@gmail.com>
2023-07-07 11:36:07 +0800

    testing

--- src/Events/Interaction/InteractionCreate.ts
+++ src/Events/Interaction/InteractionCreate.ts
@@ -4,7 +4,7 @@ import chalk from "chalk";
 import Logger from "@/Utils/Logger";
 
 export default new Event(Events.InteractionCreate, async (client, interaction) => {
-    console.log(interaction);
+    console.log({ interaction });
     // if (!client.isReady()) return;
 
     // if (interaction[0].)
