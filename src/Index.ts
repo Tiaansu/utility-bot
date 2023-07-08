de8ef594e594d870990d7693c974bab66f77d323
Tian <marlonrivera12321@gmail.com>
2023-07-08 18:05:17 +0800

    attempt to abuse free

--- src/Index.ts
+++ src/Index.ts
@@ -6,3 +6,12 @@ import Client from '@/Structures/Client';
 const client = new Client();
 
 client.start();
+
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(`<h1 style="padding: 70px 0; border: 3px solid green; text-align: center;">Hello ${req.ip}</h1>`);
+});
+
+app.listen(3000 || process.env.PORT, () => console.log(`App listening to port ${3000 || process.env.PORT}`));
